129685e68b44473d0f0ca0db7f22047ee2ddbf37
gearz <zach12342@me.com>
2020-08-23 17:57:10 -0400

    Projects pushed to github.

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ character_name = "Zach"
 character_age = int(23)
 print("There once was a man named " + character_name + ",")
 print("Zachs age is " + str(character_age) + ".")
-
+#Take input
 name = input("Enter your name: ")
 print("Hello " + name + "!")
